20dce3d5ed3b93bc44189502d5368648b7149a93
Casey Maher <cdmaher@fb.com>
2021-12-23 16:51:03 -0500

    day 20 solution

--- day20/day20.py
+++ day20/day20.py
@@ -8,63 +8,85 @@ from functools import reduce
 
 IMAGE_SIZE = 400
 
+ENHANCEMENTS = 50
+
+
 def processAlgorithmAndImage(file):
-    scanners = []
-    line = ""
-    while len(scanners) == 0 or len(beacons) > 0:
-        beacons = processBeacons(file)
-        if len(beacons) > 0:
-            scanners.append(beacons)
-    return scanners
+    algorithm = file.readline().rstrip()
+    file.readline()
+    line = file.readline().rstrip()
+    image = []
+    while line != "":
+        image.append(list(line))
+        line = file.readline().rstrip()
+    return (algorithm, image)
 
 
+def isInBounds(x, y, imageLen):
+    return y >= 0 and y < imageLen and x >= 0 and x < imageLen
 
 
-def orientations(beacon):
-    (a, b, c) = beacon
-    yield from [
-        (+a, +b, +c),
-        (+a, +c, -b),
-        (+a, -b, -c),
-        (+a, -c, +b),
-        (+b, +a, -c),
-        (+b, +c, +a),
-        (+b, -a, +c),
-        (+b, -c, -a),
-        (+c, +a, +b),
-        (+c, +b, -a),
-        (+c, -a, -b),
-        (+c, -b, +a),
-        (-a, +b, -c),
-        (-a, +c, +b),
-        (-a, -b, +c),
-        (-a, -c, -b),
-        (-b, +a, +c),
-        (-b, +c, -a),
-        (-b, -a, -c),
-        (-b, -c, +a),
-        (-c, +a, -b),
-        (-c, +b, +a),
-        (-c, -a, +b),
-        (-c, -b, -a),
-    ]
+def getNewPixelVal(algorithm, image, x, y):
+    binNum = ""
+    for i in range(y - 1, y + 2):
+        for j in range(x - 1, x + 2):
+            if not isInBounds(j, i, len(image)):
+                binNum += image[y][x]
+            else:
+                binNum += image[i][j]
+    converted = int("".join(list(map(lambda x: "0" if x == "." else "1", binNum))), 2)
+    return algorithm[converted]
 
 
-def getDifference(beaconOne, beaconTwo):
-    return tuple(map(lambda i, j: i - j, beaconTwo, beaconOne))
+def enhanceImage(algorithm, image):
+    newImage = copy.deepcopy(image)
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            val = getNewPixelVal(algorithm, image, j, i)
+            newImage[i][j] = val
+    return newImage
 
-def findCommonBeacons(firstScanner, secondScanner):
-    foundBeacons = 0
-    for
-    for x in range(len(firstScanner)):
-        for y in range(len(secondScanner)):
 
+def extendImage(image):
+    border = int((IMAGE_SIZE - len(image)) / 2)
+    for x in range(len(image)):
+        image[x] = (["."] * border) + image[x] + (["."] * border)
+    for y in range(border):
+        image.insert(0, (["."] * IMAGE_SIZE))
+        image.append((["."] * IMAGE_SIZE))
+
+
+def printImage(image):
+    for line in image:
+        for pixel in line:
+            print(pixel, end="")
+        print("")
+
+
+def numLitPixels(image):
+    numLit = 0
+    for line in image:
+        for pixel in line:
+            if pixel == "#":
+                numLit += 1
+    return numLit
+
+
+def runEnhancement(algorithm, image):
+    for x in range(ENHANCEMENTS):
+        image = enhanceImage(algorithm, image)
+    return image
 
 
 def main(fileName):
     file = open(fileName, "r")
 
-    scanners = processScanners(file)
+    (algorithm, image) = processAlgorithmAndImage(file)
+    extendImage(image)
+    printImage(image)
+    image = runEnhancement(algorithm, image)
+    printImage(image)
+    print("Num lit pixels " + str(numLitPixels(image)))
 
 
 if __name__ == "__main__":
